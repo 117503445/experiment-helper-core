3d3cd554f51d7d6a6877fa1484cf93354fc4d7d0
117503445 <t117503445@gmail.com>
2021-10-08 22:59:30 +0800

    feat: index

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-/*
- * @Author: HaoTian Qi
- * @Date: 2021-10-08 00:09:08
- * @Description:
- * @LastEditTime: 2021-10-08 00:21:05
- * @LastEditors: HaoTian Qi
- */
+module.exports = {
+  Binder: require("./src/binder"),
+  executor: require("./src/executor"),
+  util: require("./src/util"),
+};
