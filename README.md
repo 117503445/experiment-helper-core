c7ecf0396e5ba6d610cc44a9459ca84586ba1ec8
117503445 <t117503445@gmail.com>
2021-10-09 23:01:16 +0800

    style: prettier

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # experiment-helper-core
 
 实验助手 核心逻辑解析
+
+## 开发环境配置
+
+```sh
+yarn
+yarn test
+```
+
+VSCode 安装插件: [prettier](https://marketplace.visualstudio.com/items?itemName=esbenp.prettier-vscode)
--- src/experiments.js
+++ src/experiments.js
@@ -2,14 +2,10 @@
  * @Author: HaoTian Qi
  * @Date: 2021-10-09 22:18:51
  * @Description:
- * @LastEditTime: 2021-10-09 22:54:17
+ * @LastEditTime: 2021-10-09 22:57:17
  * @LastEditors: HaoTian Qi
  */
-let names = [
-  "刚体转动惯量的测量.json",
-  "平凸透镜曲率半径的测量.json",
-  "音叉振动频率的测量.json",
-];
+let names = ["刚体转动惯量的测量.json", "平凸透镜曲率半径的测量.json", "音叉振动频率的测量.json"];
 
 let experiments = names.map((value, index) => {
   return require("../res/experiments/" + value);
